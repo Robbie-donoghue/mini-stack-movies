32f0528f4c52506851a85e4631aff63256c04d1e
Robbie Donoghue <Leon.Wh1te@proton.me>
2024-02-02 22:01:53 +0000

    full functionality

--- client/main.js
+++ client/main.js
@@ -1,90 +1,81 @@
-const form = document.getElementById(`form`)
-const movieContainer = document.getElementById(`movie-container`)
-const baseURL= `http://localhost:2222`
+const form = document.getElementById(`form`);
+const movieContainer = document.getElementById(`movie-container`);
+const baseURL = `http://localhost:4242`;
 
-form.addEventListener(`submit`,async (e) =>
-{
-    const formData = new FormData(form)
-    const movieData = Object.fromEntries(formData)
-    
-    const response = await fetch(`${baseURL}/movies` , {
-        method: "POST",
-        headers : {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(movieData)
-    })
-    if(response.ok){
-        displayMovies()
-    }
-    else{
-        console.error('Failed to add movie', response.status)
-    }
-}
-)
+form.addEventListener(`submit`, async (e) => {
+  const formData = new FormData(form);
+  const movieData = Object.fromEntries(formData);
+
+  const response = await fetch(`${baseURL}/movies`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(movieData),
+  });
+  if (response.ok) {
+    displayMovies();
+  } else {
+    console.error("Failed to add movie", response.status);
+  }
+});
 
 //fetches all mvoies and returns that
-async function fetchMovies()
-{
-    const movies = await fetch(`${baseURL}/movies`)
-    //parsed into an array instead pf being json
-    let result = await movies.json()
-    return result
+async function fetchMovies() {
+  const movies = await fetch(`${baseURL}/movies`);
+  //parsed into an array instead pf being json
+  let result = await movies.json();
+  return result;
 }
 
 //display movies
 
-async function displayMovies()
-{
-    //movies is an array of all movies in my db
-    let movies = await fetchMovies()
-    movieContainer.innerHTML=''
-    //for  each movie in array
-    movies.forEach(movie =>{
-        //create images in html
-        let h3Tag= document.createElement('h3')
-        let pTag=document.createElement('p')
-        let imgTag = document.createElement('img')
-        let delButton = document.createElement('p')
-        let infoDiv = document.createElement(`div`)
-        let movieCard = document.createElement(`div`)
-        h3Tag.textContent = movie.movie
-        h3Tag.setAttribute(`class`,`movieTitle`)
-        pTag.textContent=movie.year
-        imgTag.src = movie.imgURL
-        delButton.textContent= `X`
-    })
-    // delete button
-    delButton.addEventListener(`Click ` , (e)=>
-    {
-        e.preventDefault()
-        //delete function
-        handleDelete(movie.id)
+async function displayMovies() {
+  //movies is an array of all movies in my db
+  let movies = await fetchMovies();
+  movieContainer.innerHTML = "";
+  //for  each movie in array
+  movies.forEach((movie) => {
+    //create images in html
+    let h3Tag = document.createElement("h3");
+    let pTag = document.createElement("p");
+    let imgTag = document.createElement("img");
+    let delButton = document.createElement("p");
+    let infoDiv = document.createElement(`div`);
+    let movieCard = document.createElement(`div`);
+    h3Tag.textContent = movie.movie;
+    h3Tag.setAttribute(`class`, `movieTitle`);
+    pTag.textContent = movie.year;
+    imgTag.src = movie.imgURL;
+    delButton.textContent = `X`;
 
-    }
-    )
-    infoDiv.appendChild(h3Tag)
-    infoDiv.appendChild(pTag)
-    movieCard.appendChild(delButton)
-    movieCard.appendChild(infoDiv)
-    movieCard.appendChild(imgTag)
-    
-    movieContainer.appendChild(movieCard)
+    // delete button
+    delButton.addEventListener(`Click `, (e) => {
+      e.preventDefault();
+      //delete function
+      handleDelete(movie.id);
+    });
+    infoDiv.appendChild(h3Tag);
+    infoDiv.appendChild(pTag);
+    movieCard.appendChild(delButton);
+    movieCard.appendChild(infoDiv);
+    movieCard.appendChild(imgTag);
+
+    movieContainer.appendChild(movieCard);
+  });
 }
 
 //handle delete async function
-async function handleDelete(id)
-{
-    const result = await fetch(`${baseURL}/movies/${id}`,{
-        method:"DELETE"
-    })
-    //log result
-    console.log(results)
-    if(result.ok)
-    {
-        displayMovies()
-    }
+async function handleDelete(id) {
+  const result = await fetch(`${baseURL}/movies/${id}`, {
+    method: "DELETE",
+  });
+  //log result
+  console.log(results);
+  if (result.ok) {
+    displayMovies();
+  }
 }
 
 //display movie function
-displayMovies()
+displayMovies();
--- server/server.js
+++ server/server.js
@@ -1,68 +1,60 @@
-import express from "express"
-import cors from "cors"
-import Database from "better-sqlite3"
+import express from "express";
+import cors from "cors";
+import Database from "better-sqlite3";
 
-const PORT = `4242`
-const app = express()
-const db = new Database('database.db')
-app.use(express.json())
+const PORT = `4242`;
+const app = express();
+const db = new Database("database.db");
+app.use(express.json());
+app.use(cors());
 
-app.listen(`${PORT}`  , () => {
-    console.log(`Server started on PORT: ${PORT}`)
-})
+app.listen(`${PORT}`, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
 
 //get request, ROOT ROUTE
-app.get(`/` ,  (req, res) => 
-{
-    res.send(`OooooOOhh!`)
-})
+app.get(`/`, (req, res) => {
+  res.send(`OooooOOhh!`);
+});
 
-//database request 
-app.get('/movies' , (req,res) => 
-{
-    try 
-    {
-        // find a record by it's id
-        if(req.query.id)
-        {
-        let movie =db.prepare(`SELECT * FROM movies WHERE id = ?`).all(req.query.id)
-       //prepare() only gets run when we run .all() 
-        res.status(200).json(movie)
-        return
-        }
-        let movies = db.prepare(`SELECT * FROM movies`).all()
-        // prepare()optimizes statement for use 
-        // .all()runs the statement, and return all results
+//database request
+app.get("/movies", (req, res) => {
+  try {
+    // find a record by it's id
+    if (req.query.id) {
+      let movie = db
+        .prepare(`SELECT * FROM movies WHERE id = ?`)
+        .all(req.query.id);
+      //prepare() only gets run when we run .all()
+      res.status(200).json(movie);
+      return;
     }
-    catch(err)
-    {
-        res.status(500).json(err)
-    }
-    if(req.query.one)
-    {
-        res.send(`Query one`)
-    }
-})
-
+    let movies = db.prepare(`SELECT * FROM movies`).all();
+    // prepare()optimizes statement for use
+    // .all()runs the statement, and return all results
+    res.status(200).json(movies);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  if (req.query.one) {
+    res.send(`Query one`);
+  }
+});
 
 // POST route to create new movie entries in our database
-app.post('/movies' , (req,res) =>
-{
-    try 
-    {
-        const movie = req.body.movie
-        const year = req.body.year
-        const imgURL= req.body.imgURL
-        // run sql statement
-        const newMovie = db.prepare(`INSERT INTO movies (movie , year , imgURL) VALUES(?,?, ?)`).run(movie, year , imgURL)
-        res.status(200).json(newMovie)
-
-    }
-    catch(err)
-    {
-        res.status(500).json({error : err})
-    }
-})
-
+app.post("/movies", (req, res) => {
+  try {
+    const movie = req.body.movie;
+    const year = req.body.year;
+    const imgURL = req.body.imgURL;
+    // run sql statement
+    const newMovie = db
+      .prepare(`INSERT INTO movies (movie , year , imgURL) VALUES(?,?, ?)`)
+      .run(movie, year, imgURL);
+    res.status(200).json(newMovie);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
 // delete and update next
